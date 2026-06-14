ab995bdae4ce2ec9a49dc6f34afa7425d3f3cbf5
Dhruba Karim <zkdhruba@gmail.com>
2026-06-14 11:22:13 +0600

    121 my solution - time limit exceed

--- 121_best_time_to_buy_sell_stocks.js
+++ 121_best_time_to_buy_sell_stocks.js
@@ -1,17 +1,19 @@
-const prices = [7, 1, 5, 3, 6, 4];
+const prices = [2, 4, 1];
 
 var maxProfit = function (prices) {
   const arrLen = prices.length;
   let lowest = prices[0];
   let profit = 0;
   for (i = 0; i < arrLen; i++) {
-    if (prices[i] - lowest > profit) {
-      profit = prices[i] - lowest;
+    for (j = i; j < arrLen; j++) {
+      if (prices[j] - lowest > profit) {
+        profit = prices[j] - lowest;
+      }
     }
+    lowest = prices[i + 1];
   }
-  console.log(profit);
 
-  //   return highestAfterLowest - lowest;
+  return profit;
 };
 
 maxProfit(prices);
